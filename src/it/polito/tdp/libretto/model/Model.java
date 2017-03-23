50f65d972eb3459054d9d0667808f52ecc97ffce
gianni arigliani <gianni arigliani@gianni.homenet.telecomitalia.it>
2017-03-23 14:12:08 +0100

    Model

--- src/it/polito/tdp/libretto/model/Model.java
+++ src/it/polito/tdp/libretto/model/Model.java
@@ -1,5 +1,43 @@
 package it.polito.tdp.libretto.model;
 
-public class Model {
+import java.util.*;
 
+public class Model {
+     private List<Esame> esami;
+     
+     public Model(){
+    	 this.esami=new ArrayList<Esame>();
+     }
+     
+     /**
+      * aggiungo un esame all elenco degli esami presenti,
+      * verificando che non ci sia.
+      * Non si lascia mai il silent failor, si mette quindi o che ritorna un buoolean, oppure 
+      * che mi fa partire l' eccezione
+      * @param e
+      * @return true se l' hainserito, false se gia esistente e quindi non lo aggiunge
+      */
+    public boolean addEsame(Esame e){
+    	if(!esami.contains(e)){
+    		esami.add(e);
+    		return true;
+    	}
+    	else{
+    		return false;
+    	}
+    }
+    
+    public Esame trovaEsame( String codice){
+    	
+    	//codì mi cerca l' esame che vglio, l ho potuto fare perche il mio equals si basa solo sul codice
+    	int pos=this.esami.indexOf(new Esame(codice, null, null));
+    	if(pos==-1){
+    		return null;
+    	}
+    	else{
+    		return esami.get(pos);
+    	}
+    	
+    	
+    }
 }
---
+++ src/it/polito/tdp/libretto/model/TestModel.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.libretto.model;
+
+public abstract class TestModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
