7571d933992d751aafe0257e87fb8350b00f2604
gianni arigliani <gianni arigliani@gianni.homenet.telecomitalia.it>
2017-03-23 13:42:35 +0100

    classe "Esame"

---
+++ src/it/polito/tdp/libretto/model/TestEsame.java
@@ -0,0 +1,9 @@
+package it.polito.tdp.libretto.model;
+
+public class TestEsame {
+    public static void main (String[] args){
+    	// qui dentro fai la prova come
+    	//ad oggetto
+    	
+    }
+}
