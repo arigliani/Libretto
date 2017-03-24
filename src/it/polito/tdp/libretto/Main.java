75488b5d7f87171f9d119f83b973c3bf4f7cb2ae
gianni arigliani <gianni arigliani@gianni.homenet.telecomitalia.it>
2017-03-24 09:47:20 +0100

    finito controller

--- src/it/polito/tdp/libretto/Main.java
+++ src/it/polito/tdp/libretto/Main.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.libretto;
 	
+import it.polito.tdp.libretto.model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,9 +11,15 @@ import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Libretto.fxml"));
-			Scene scene = new Scene(root,400,400);
+		try {// questo va semmpre fatto così
+			FXMLLoader loader= new  FXMLLoader(getClass().getResource("Libretto.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			
+			LibrettoController controller=loader.getController();
+			Model model=new Model();
+			controller.setModel(model); //la classe e creata da FXMLLloader perciò non posso creare un oggetto
+			
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
